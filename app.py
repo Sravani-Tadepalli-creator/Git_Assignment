0f379192f839f38988081a76406ebec24bb51991
Sravani Tadepalli <sravani.tadepalli@irissoftware.com>
2026-05-10 16:46:03 +0530

    added area of recctangle function

--- app.py
+++ app.py
@@ -13,3 +13,17 @@ class Exercise1:
 person1 = Exercise1('Sravani', 38)
 person1.display_info()
 
+class Exercise2:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        return self.length * self.breadth
+
+    def perimeter(self):
+        return (2*(self.length + self.breadth))
+
+rectangle = Exercise2(20, 10)
+print(f'Area is {rectangle.area()}')
+print(f'Perimeter is {rectangle.perimeter()}')
